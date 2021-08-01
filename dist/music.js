36a3a4f524c7fbac100524876b3d8ccff7c69678
wenhao-says <2294548755@qq.com>
2021-08-01 17:54:50 +0800

    Site updated: 2021-08-01 17:54:50

--- dist/music.js
+++ dist/music.js
@@ -2,7 +2,7 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'), //播放器容器元素
     mini: false, //迷你模式
     fixed: false, //吸底且靠右，在最底部
-    autoplay: true, //自动播放
+    autoplay: false, //自动播放
     theme: '#FADFA3', //主题色
     loop: 'all', //音频循环播放, 可选值: 'all'全部循环, 'one'单曲循环, 'none'不循环
     order: 'random', //音频循环顺序, 可选值: 'list'列表循环, 'random'随机循环
